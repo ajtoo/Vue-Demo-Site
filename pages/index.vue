2d7c515733fbe11601275ebeddad279a972f9b46
ajtoo <ajtoo.dev@gmail.com>
2018-01-06 10:38:53 -0800

    Implement ImageSlider and remove the default project logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,18 @@
 <template>
   <section class="container">
     <navbar/>
-    <div>
-      <logo/>
-      <h1 class="title">
-        vue-demo-site
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <image-slider/>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 import Navbar from '~/components/Navbar.vue'
+import ImageSlider from '~/components/ImageSlider.vue'
 
 export default {
   components: {
-    Logo,
-    Navbar
+    Navbar,
+    ImageSlider
   }
 }
 </script>
